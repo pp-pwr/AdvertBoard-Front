22c6e6f41a856f9c5f9409bf67b622ff2a9e6180
eipc16 <pprzemek.312@gmail.com>
2019-03-15 16:43:20 +0100

    added components for routing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import MainPage from "./components/pages/MainPage"
+import Login from "./components/pages/Login"
+import Registration from "./components/pages/Registration"
+import NotFound from "./components/pages/NotFound"
+import Profile from "./components/pages/Profile"
 
 class App extends Component {
-
-  state = {};
-
-  componentDidMount() {
-    setInterval(this.hello, 150);
-  }
-
-  hello = () => {
-    fetch('/api/hello')
-      .then(response => response.text())
-      .then(message => {
-        this.setState({message: message});
-      });
-  };
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.message}
-          </p>
-        </header>
-      </div>
-    );
+      <BrowserRouter style={{height: '100%'}}>
+        <div style={{height: '100%'}}>
+          <Switch className="bg-dark" style={{height: '100%'}}>
+            <Route path="/" component={MainPage} exact />
+            <Route path="/login" component={Login} exact />
+            <Route path="/register" component={Registration} exact />
+            <Route path="/profile" component={Profile} exact />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
   }
 }
 
---
+++ src/components/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react"
+import '../../styles/MainPage.scss'
+
+class MainPage extends Component {
+
+    render() {
+        return (
+            <div>MainPage</div>
+        )
+    }
+}
+
+export default MainPage;
