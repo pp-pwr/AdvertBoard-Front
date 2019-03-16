ee96becb2aaf49c7d8733d2018e073d7725df660
eipc16 <pprzemek.312@gmail.com>
2019-03-16 12:16:41 +0100

    added navigation bar

--- src/common/NavigationBar.js
+++ src/common/NavigationBar.js
@@ -6,9 +6,28 @@ import "./NavigationBar.css"
 class NavigationBar extends Component {
     render() {
         return (
-            <div>
-                Nic tutaj póki co
-            </div>
+            <header className="app-header">
+                <div className="container">
+                    <div className="app-main-page">
+                        <Link to="/" className="app-title">AdvertBoard</Link>
+                    </div>
+                    <div className="app-header-elements">
+                        <nav className="app-header-navigation">
+                            { this.props.authenticated ? (
+                                <ul>
+                                    <li><NavLink to="/profile">Mój profil</NavLink></li>
+                                    <li><button onClick={ this.props.onLogout }></button></li>
+                                </ul>
+                            ) : (
+                                <ul>
+                                    <li><NavLink to="/login">Logowanie</NavLink></li>
+                                    <li><NavLink to="/signup">Rejestracja</NavLink></li>
+                                </ul>
+                            )}
+                        </nav>
+                    </div>
+                </div>
+            </header>
         );
     }
 }
