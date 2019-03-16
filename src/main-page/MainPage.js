3c899a73051c20c827007c77ac2072e301817ddc
eipc16 <pprzemek.312@gmail.com>
2019-03-16 12:28:48 +0100

    added main page .js and .css files

---
+++ src/main-page/MainPage.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './MainPage.css';
+
+class MainPage extends Component {
+    render() {
+        return (
+            <div className="main-page-container">
+                <div className="container">
+                    <div className="graf-bg-container">
+                        <div className="graf-layout">
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                        </div>
+                    </div>
+                    <h1 className="main-page-title">Test</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MainPage;
