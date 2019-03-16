79aff39a8b3bf4d97fc02fc71784ecc728fe2f98
eipc16 <pprzemek.312@gmail.com>
2019-03-16 11:47:04 +0100

    fixes with NotFound.js

--- src/common/NotFound.js
+++ src/common/NotFound.js
@@ -1,13 +1,25 @@
 import React, {Component} from "react"
+import { Link } from "react-router-dom"
+
+import "./NotFound.css"
 
 class NotFound extends Component {
     render() {
         return (
-            <div>
-                <h1>Error 404: Page not found</h1>
-                <h2>There's nothing here!</h2>
+            <div className="page-not-found">
+                <h1 className="title">
+                    Błąd 404
+                </h1>
+                <div className="desc">
+                    Strona o podanym adresie nie istnieje!
+                </div>
+                <Link to="/">
+                    <button className="go-back-btn btn btn-primary" type="button">
+                        Wróć
+                    </button>
+                </Link>
             </div>
-        )
+        );
     }
 }
 
