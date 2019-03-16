c517f6030bce6c6b73d2fbf0016b107e31569111
eipc16 <pprzemek.312@gmail.com>
2019-03-16 11:46:44 +0100

    added placeholder for top navigation bar

---
+++ src/common/NavigationBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+import { Link, NavLink } from "react-router-dom"
+
+import "./NavigationBar.css"
+
+class NavigationBar extends Component {
+    render() {
+        return (
+            <div>
+                Nic tutaj póki co
+            </div>
+        );
+    }
+}
+
+export default NavigationBar
