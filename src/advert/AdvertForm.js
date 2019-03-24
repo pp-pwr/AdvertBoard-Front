f6e712325ac6e10ce3f8997dd8d4d8d9d24444d2
mateuszp <mpftp10@gmail.com>
2019-03-24 15:18:57 +0100

    defined advertform constructor and methods

--- src/advert/AdvertForm.js
+++ src/advert/AdvertForm.js
@@ -1,8 +1,49 @@
-import React, { Component } from "react"
-import { Link, Redirect } from "react-router-dom"
-import Alert from "react-s-alert"
+import React, {Component} from "react"
+import {getCategories} from "../utils/APIUtils"
 
 class AdvertForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            tags: {},
+            description: '',
+            imgUrls: {},
+            subcategory: ''
+        }
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
 
+    componentDidMount() {
 
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const inputName = target.name;
+        const inputValue = target.value;
+
+        this.setState({[inputName]: inputValue})
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" name="title" placeholder="Tytuł"
+                       value={this.state.title} onChange={this.handleInputChange} required/>
+                <br/>
+                <textarea name="description" placeholder="Opis"
+                          value={this.state.description} onChange={this.handleInputChange} required/>
+            </form>
+
+        )
+    }
 }
+
+export default AdvertForm
