30454a3b110e8a5e84f0b92f5766a209f19e1ad0
eipc16 <pprzemek.312@gmail.com>
2019-04-09 16:12:33 +0200

    changed urls to heroku

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,8 @@ export const API_BASE_URL = '/api'
 export const API_TRUE_URL = 'https://pwr-advert-board-backend.herokuapp.com'
 export const ACCESS_TOKEN = 'accessToken';
 
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
+//export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
+export const OAUTH2_REDIRECT_URI = 'http://pwr-advert-board-frontend.herokuapp.com/oauth2/redirect'
 
 export const GOOGLE_AUTH_URL = API_TRUE_URL + API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_TRUE_URL + API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
