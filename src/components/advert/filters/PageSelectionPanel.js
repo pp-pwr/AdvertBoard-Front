c9ff0a1dc1ef8a0c2b4140e3cbad5f4cbdb58b4f
eipc16 <pprzemek.312@gmail.com>
2019-06-02 21:22:54 +0200

    style changes

--- src/components/advert/filters/PageSelectionPanel.js
+++ src/components/advert/filters/PageSelectionPanel.js
@@ -10,6 +10,29 @@ class PageSelectionPanel extends Component {
 
         this.createButtons = this.createButtons.bind(this)
         this.notifyParent = this.notifyParent.bind(this)
+
+        this.state = {
+            width: 0,
+            height: 0
+        }
+
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this)
+    }
+
+    componentDidMount() {
+        this.updateWindowDimensions()
+        window.addEventListener('resize', this.updateWindowDimensions)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions)
+    }
+
+    updateWindowDimensions() {
+        this.setState({
+            width: window.innerWidth,
+            height: window.innerHeight
+        })
     }
 
     notifyParent() {
@@ -33,14 +56,25 @@ class PageSelectionPanel extends Component {
         let buttons = []
 
         let page_limit = 10;
+        let show_add_btns = true
+
+        if(this.state.width < 1100) {
+            page_limit = 4;
+            show_add_btns = false
+        } else if (this.state.width < 600) {
+            page_limit = 1;
+            show_add_btns = false
+        }
 
         let className = 'page-numbers';
 
-        buttons.push(
-            <li key={ "page_first" } onClick={() => this.setPage(0)} className='page-numbers'>
-                <button>Pierwsza</button>
-            </li>
-        )
+        if(this.currentPage > 0) {
+            buttons.push(
+                <li key={ "page_first" } onClick={() => this.setPage(0)} className='page-numbers'>
+                    <button>Pierwsza</button>
+                </li>
+            )
+        }
 
         let i_lower = Math.max(0, this.currentPage - (page_limit / 2))
         let i_upper = Math.min(this.props.pages, this.currentPage + (page_limit / 2)) 
@@ -67,11 +101,13 @@ class PageSelectionPanel extends Component {
             )
         }
 
-        buttons.push(
-            <li key={ "page_first" } onClick={() => this.setPage(this.props.pages - 1)} className='page-numbers'>
-                <button>Ostatnia</button>
-            </li>
-        )
+        if(this.currentPage < this.props.pages - 1) {
+            buttons.push(
+                <li key={ "page_first" } onClick={() => this.setPage(this.props.pages - 1)} className='page-numbers'>
+                    <button>Ostatnia</button>
+                </li>
+            )
+        }
 
         return buttons
     }
